e5247773454141029310dd6920afb239f5bbbdf3
ritesh123malik <127617308+ritesh123malik@users.noreply.github.com>
2026-04-05 17:17:58 +0530

    Sync: Two Sum - LeetCode

--- LeetCode/Two_Sum_-_LeetCode.py
+++ LeetCode/Two_Sum_-_LeetCode.py
@@ -1,1 +1,21 @@
-c
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i;
+        int j;
+        vector <int> f;
+        int n=nums.size();
+        int rem=0;
+        for(i=0;i<n;i++){
+            rem=target-nums[i];
+            for(j=i+1;j<n;j++){
+                if(nums[j]==rem){
+                    f={i,j};
+                    
+                    
+                }
+            }
+        }
+        return f;
+    }
+};
